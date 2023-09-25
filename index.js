56fa96dde926c7e7243148bf5d643c064635fb9b
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-25 14:43:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// Function to find drivers with an exact name match
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to find drivers with names that start with a provided string
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+  }
+  
+  // Function to find drivers with a matching name
+  function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+  }
+
+  
